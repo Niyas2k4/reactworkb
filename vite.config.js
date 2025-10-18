7dd7997ef2d53c540711be084e8d1defe74bdec0
Niyas2k4 <nusaibatk66.com>
2025-10-18 13:58:46 +0530

    fix vite base path

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { HashRouter } from 'react-router-dom';
+
 
 export default defineConfig({
   plugins: [react()],
