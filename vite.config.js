22eb698093909a86cfc4d2d80235eb91f6595d91
Niyas2k4 <aswertpa@gmail.com>
2025-10-18 13:41:12 +0530

    Update vite.config.js for new root

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/reactworkb/', // 👈 your repo name here
+  base: '/Portpholio/', // 👈 your repo name here
 })
